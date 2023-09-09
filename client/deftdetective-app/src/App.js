2a74f1962447e80e65f2d5e63c036d0e10f12b23
AjlaMah <ajla.lux@gmail.com>
2023-09-09 14:38:47 +0200

    landing page added

--- client/deftdetective-app/src/App.js
+++ client/deftdetective-app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="landing-page">
+      <div className="logo">
+        {/* Create a circle using CSS */}
+        <div className="circle"></div>
+      </div>
+      <h1>Deft Detective</h1>
+      <button className="lets-go-button">Let's Go</button>
+      <p className="login-button">Already a user? <a href="/login">Log in!</a></p>
     </div>
   );
 }
