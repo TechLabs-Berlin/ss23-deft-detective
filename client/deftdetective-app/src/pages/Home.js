6801bf17f057e19b1cdf2ff0343711390cf730b2
AjlaMah <ajla.lux@gmail.com>
2023-09-26 14:51:52 +0200

    whatpage framework (doesnt send info to be yet)

--- client/deftdetective-app/src/pages/Home.js
+++ client/deftdetective-app/src/pages/Home.js
@@ -1,19 +1,3 @@
-// import React from 'react';
-// import { useNavigate } from 'react-router-dom'
-// import './Home.css';
-// import { Link } from "react-router-dom";
-
-// function Home() {
-//     return (
-//         <div className="landing-page">
-//             <div className="logo">
-//                 <div className="circle"></div>
-//             </div>
-//             <h1>Deft Detective</h1>
-//             <button className="lets-go-button">Let's Go</button>
-//             <p className="login-button">Already a user? <a href="/what">Log in!</a></p>
-//         </div>
-
 import { Link } from "react-router-dom"
 import './Home.css';
 
@@ -21,14 +5,14 @@ export function Home() {
     return (
         <>
             <div className="landing-page">
-                <div className="logo">
-                    <div className="circle"></div>
-                </div>
+
+                <div className="circle"></div>
+
                 <h2>Email nearby businesses about your lost item in just three easy steps</h2>
                 <div className="stepscircle">
-                    <div className="circle-small">1</div>
-                    <div className="circle-small">2</div>
-                    <div className="circle-small">3</div>
+                    <div className="circle-small"><h3>1</h3></div>
+                    <div className="circle-small"><h3>2</h3></div>
+                    <div className="circle-small"><h3>3</h3></div>
                 </div>
                 <Link to="/what"><button className="lets-go-button" >Let's Go!</button></Link>
             </div>
--- client/deftdetective-app/src/pages/What.js
+++ client/deftdetective-app/src/pages/What.js
@@ -1,10 +1,107 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import './WhatWhenWhere.css';
 
 export function What() {
+    const [formData, setFormData] = useState({
+        itemName: '',
+        description: '',
+        picture: null,
+    });
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handlePictureChange = (event) => {
+        const pictureFile = event.target.files[0];
+        setFormData((prevData) => ({
+            ...prevData,
+            picture: pictureFile,
+        }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (event.target.checkValidity()) {
+            // TODO: Add form submission logic, e.g., API request or database insertion
+            console.log('Form Data:', formData);
+            setFormData({
+                itemName: '',
+                description: '',
+                picture: null,
+            });
+        } else {
+            console.log('Form is not valid');
+        }
+    };
+
     return (
         <>
-            <h1>What</h1>
-            <Link to="/when"><button>Next</button></Link>
+            <nav className="navbar"><div className="circle-navlogo"></div></nav>
+            <div className="what-page">
+                <div className="stepscircle">
+                    <div className="circle-small-one"><h3>1</h3></div>
+                    <div className="circle-small-two"><h3>2</h3></div>
+                    <div className="circle-small-three"><h3>3</h3></div>
+                </div>
+                <h1>What?</h1>
+                <h2>Let us know what's missing. Be as specific as you can.</h2>
+                <div className="what-submit">
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <p>Item Name</p>
+                            <label>
+                                <input
+                                    type="text"
+                                    name="itemName"
+                                    value={formData.itemName}
+                                    onChange={handleInputChange}
+                                    required
+                                />
+                            </label>
+                        </div>
+                        <div>
+                            <p>Description</p>
+                            <label><textarea
+                                name="description"
+                                value={formData.description}
+                                onChange={handleInputChange}
+                                required
+                            />
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <input
+                                    type="file"
+                                    name="picture"
+                                    accept="image/*"
+                                    onChange={handlePictureChange}
+                                />
+                                <input
+                                    type="file"
+                                    name="picture"
+                                    accept="image/*"
+                                    onChange={handlePictureChange}
+                                />
+                                <input
+                                    type="file"
+                                    name="picture"
+                                    accept="image/*"
+                                    onChange={handlePictureChange}
+                                />
+                            </label>
+                        </div>
+                        <Link to="/when"><button className="next">Next</button></Link>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
