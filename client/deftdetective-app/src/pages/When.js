b499e7e52bc7d5317eed12478a8b899005654bf0
AjlaMah <ajla.lux@gmail.com>
2023-09-28 14:57:36 +0200

    whenpage structure done

--- client/deftdetective-app/src/pages/When.js
+++ client/deftdetective-app/src/pages/When.js
@@ -7,7 +7,7 @@ export function When() {
     const [formData, setFormData] = useState({
         date: '',
         time: '',
-        picture: null,
+        timeOption: '',
     });
 
     const handleInputChange = (event) => {
@@ -27,6 +27,7 @@ export function When() {
             setFormData({
                 date: '',
                 time: '',
+                timeOption: '',
             });
         } else {
             console.log('Form is not valid');
@@ -56,25 +57,100 @@ export function When() {
                                     onChange={handleInputChange}
                                     required
                                 />
+                                -
+                                <input
+                                    type="date"
+                                    name="date"
+                                    value={formData.date}
+                                    onChange={handleInputChange}
+                                    required
+                                />
                             </label>
                         </div>
+                        <p>Time</p>
+                        <div className="timeOption">
+                            <label>
+                                <input
+                                    type="radio"
+                                    name="timeOption"
+                                    value="12 AM - 4 AM"
+                                    checked={formData.timeOption === '12 AM - 4 AM'}
+                                    onChange={handleInputChange}
+                                    required
+                                /> <span className="timeOptionText">12 AM - 4 AM</span>
+                            </label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    name="timeOption"
+                                    value="4 AM - 8 AM"
+                                    checked={formData.timeOption === '4 AM - 8 AM'}
+                                    onChange={handleInputChange}
+                                    required
+                                /> <span className="timeOptionText">4 AM - 8 AM</span>
+                            </label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    name="timeOption"
+                                    value="8 AM - 12 PM"
+                                    checked={formData.timeOption === '8 AM - 12 PM'}
+                                    onChange={handleInputChange}
+                                    required
+                                /><span className="timeOptionText">8 AM - 12 PM</span>
+                            </label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    name="timeOption"
+                                    value="12 PM - 4 PM"
+                                    checked={formData.timeOption === '12 PM - 4 PM'}
+                                    onChange={handleInputChange}
+                                    required
+                                /><span className="timeOptionText">12 PM - 4 PM</span>
+                            </label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    name="timeOption"
+                                    value="4 PM - 8 AM"
+                                    checked={formData.timeOption === '4 PM - 8 PM'}
+                                    onChange={handleInputChange}
+                                    required
+                                /><span className="timeOptionText">4 PM - 8 PM</span>
+                            </label>
+                            <label>
+                                <input
+                                    type="radio"
+                                    name="timeOption"
+                                    value="8 PM - 12 AM"
+                                    checked={formData.timeOption === '8 PM - 12 AM'}
+                                    onChange={handleInputChange}
+                                    required
+                                /><span className="timeOptionText">8 PM - 12 AM</span>
+                            </label>
+                        </div>
+                        <p>or</p>
                         <div>
-                            <p>Time</p>
-                            <label><textarea
-                                name="time"
-                                value={formData.time}
-                                onChange={handleInputChange}
-                                required
-                            />
+                            <label>
+                                <input
+                                    type="radio"
+                                    name="timeOption"
+                                    value="IDontKnow"
+                                    checked={formData.timeOption === 'IDontKnow'}
+                                    onChange={handleInputChange}
+                                    required
+                                /><span className="timeOptionText">I don't know</span>
                             </label>
                         </div>
                         <div className="buttons">
-                        <Link to="/what"><button className="back">Back</button></Link>
-                        <Link to="/where"><button className="next" type="submit">Next</button></Link>
+                            <Link to="/what"><button className="back">Back</button></Link>
+                            <Link to="/where"><button className="next" type="submit">Next</button></Link>
                         </div>
                     </form>
                 </div>
-            </div>
+
+            </div >
         </>
     )
 }
