cab7789e73d209439852943de12aea68f6fec4b3
Guilherme Mazzolini <guilhermemazzolini@hotmail.com>
2023-10-05 16:17:25 +0200

    map

--- client/deftdetective-app/src/App.js
+++ client/deftdetective-app/src/App.js
@@ -6,6 +6,8 @@ import { Where } from "./pages/Where"
 import { Preview } from "./pages/Preview"
 import { SignUp } from "./pages/SignUp"
 import { SubmitMessage } from "./pages/SubmitMessage"
+// import MapPage from './components/Map'
+// import CoordinatesDisplay from './components/CoordinatesDisplay'
 
 function App() {
   return (
@@ -14,6 +16,11 @@ function App() {
       <Route path="/what" element={<What />} />
       <Route path="/when" element={<When />} />
       <Route path="/where" element={<Where />} />
+      {/* <Route path="/where" element={<MapPage />} />
+          <Route path="/where" element={<CoordinatesDisplay />} />
+          <Route path="/auth" element={<Register />}>
+            <Route index element={<Login />} />
+          </Route> */}
       <Route path="/preview" element={<Preview />} />
       <Route path="/SignUp" element={<SignUp />} />
       <Route path="/SubmitMessage" element={<SubmitMessage />} />
