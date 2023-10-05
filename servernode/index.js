47b0b5d8388eb937fcb8bc7b68cbcdfb673bc3c5
Guilherme Mazzolini <guilhermemazzolini@hotmail.com>
2023-10-05 16:31:06 +0200

    map

---
+++ servernode/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const path = require('path')
+const bodyParser = require('body-parser');
+
+app.use(express.static(path.join(__dirname + "/public")))
+app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(bodyParser.json());
+
+
+const mongoose = require('mongoose');
+
+require("dotenv").config();
+
+mongoose.connect("mongodb://localhost:27017/deft-detective")
+.then((x) => {
+    const dbName = x.connections[0].name;
+    console.log(`Connected to Mongo! Database name: "${dbName}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
+
+const index = require('./routes/index')
+app.use('/', index)
+
+const authRouter = require('./routes/auth.routes')
+app.use('/', authRouter)
+
+const PORT = process.env.PORT || 1000
+
+app.listen(PORT, console.log(`server listening on port ${PORT}`))
